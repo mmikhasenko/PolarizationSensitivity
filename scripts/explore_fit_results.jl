6e69c6805a7b598027da8a4a99f51e6f7fed01a9
Misha Mikhasenko <mikhail.mikhasenko@gmail.com>
2020-09-07 14:25:13 +0200

    prototyped fit exploration

--- scripts/explore_fit_results.jl
+++ scripts/explore_fit_results.jl
@@ -19,6 +19,32 @@ tfr = Table(
         for fr in settings["fit_results"]])
 print(tfr)
 
+# TODO:
+# - move to utils
+arg(z) = atan(imag(z), real(z))
+
+# add tests
+a = -1-1im
+atan(imag(a) / real(a)) / π * 180 # ∈ [-π/2, π/2]
+atan(imag(a), real(a)) / π * 180 # ∈ [-π, π]
+arg(p[1]'*p[3]) # is phase of 3 - phase of 1
+
+# implement
+import PolarizationSensitivity: α1
+α1(pars) = α1(pars, isobars)
+# α2(pars, isobars)
+# α3(pars, isobars)
+
+# add the plot for fit attemps
+α1v = α1.(tfr.pars)
+histogram(α1v, xlab="α₁")
+
+# 
+# p3 = |p3|*exp(iϕ3)
+# p1 = |p1|*exp(iϕ1)
+# p1'*p3 = |p3|*|p1|*exp(iϕ3-iϕ1)
+
+
 # converged = filter(x->x.st, tfr);
 
 # print(converged)
