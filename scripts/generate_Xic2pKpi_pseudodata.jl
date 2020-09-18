68d48d87a5775885c6b663ad2129b321927ed9d3
Vdedu <vlad-george.dedu@cern.ch>
2020-09-18 13:28:21 +0300

    New model, 6 params  and cleaning

--- scripts/generate_Xic2pKpi_pseudodata.jl
+++ scripts/generate_Xic2pKpi_pseudodata.jl
@@ -3,18 +3,19 @@ using Plots.StatsBase
 using PartialWaveFunctions
 using ThreeBodyDecay
 using QuadGK
+using DelimitedFiles
 
 using PolarizationSensitivity
 
 import PolarizationSensitivity: intensity, interference, ellh
 intensity(σs; pars) = intensity(σs, isobars; pars=pars)
 
+#Set-up model
 tbs = tbs_Ξc2pKπ
-isobars = (Kst872_pc, Kst872_pv, Λ1520_pc, Λ1520_pv, Δ1232_pc, Δ1232_pv)
+const isobars = (Kst872_pc, Kst872_pv, Δ1232_pc, Δ1232_pv,Λ1520_pc, Λ1520_pv)
 
-
-
-let pars = [1, 0, 1, 0, 1, 0]
+#Dalitz plot for genpars
+let pars = [1.3, 1 - 1im, 1.2 - 0.5im, 2 + 0.3im, 2 - 0.6im, 2 + 1im]
     σ3v = range(lims3(tbs.ms)..., length=100)
     σ1v = range(lims1(tbs.ms)..., length=100)
     cal = [
@@ -25,10 +26,12 @@ let pars = [1, 0, 1, 0, 1, 0]
         color=cgrad(:viridis, scale=:exp),
         xlab="σ₁ ≡ m²(Kπ) (GeV)",
         ylab="σ₃ ≡ m²(pK) (GeV)")
+        savefig("Dalitz.pdf")
 end
 
 
-const genpars = [1, 1, 1.1, 3.9, 2, 2]
+const genpars = [1.3, 1 - 1im, 1.2 - 0.5im, 2 + 0.3im, 2 - 0.6im, 2 + 1im]
+
 
 function generate_sample(Intensity; Nev)
     s = flatDalitzPlotSample(tbs.ms; Nev=Nev)
@@ -40,17 +43,11 @@ end
 
 @time generate_sample(σs->intensity(σs; pars=genpars); Nev=20_000)
 
-
 const gs_large = generate_sample(σs->intensity(σs; pars=genpars); Nev=100_000);
 
-using DelimitedFiles
-writedlm(joinpath("data","sims","sample_Kstar=1,1_Lambda=1.1,3.9_Delta=2,2.txt"),
+writedlm(joinpath("data","sims","sample_Kstar=1.3,1-1im_Lambda=1.2-0.5im, 2+0.3im_Delta=2-0.6im,2+1im.txt"),
     [getproperty.(gs_large,:σ1) getproperty.(gs_large,:σ3)])
 
+#plot Dalitz of data
 histogram2d(getproperty.(gs_large,:σ1), getproperty.(gs_large,:σ3), bins=50)
 
-# reading
-let
-    M = readdlm(joinpath("data","sims","sample_Kstar=1,1_Lambda=1.1,3.9_Delta=2,2.txt"))
-    [Invariants(tbs.ms,σ1=M[i,1],σ3=M[i,2]) for i in 1:size(M,1)]
-end
